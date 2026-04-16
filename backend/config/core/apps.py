e604c15b349e8fa1ecdc3b020ae9c0e784e9c647
Mahmudul Hasan Hasib <125702889+mahmudul-hasan-hasib@users.noreply.github.com>
2026-04-17 00:38:09 +0600

    add auto superuser creation

--- backend/config/core/apps.py
+++ backend/config/core/apps.py
@@ -2,4 +2,16 @@ from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = "core"
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@gmail.com",
+                password="admin123"
+            )
